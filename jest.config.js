bc2b2830513731998b096c9777db20cb54c4d44f
EkremKirdemir <kirdemir.ekrem@gmail.com>
2024-12-19 17:30:51 +0300

    test removed

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: 'react-native',
-    setupFilesAfterEnv: ['./jest.setup.js'],
-    transformIgnorePatterns: [
-      'node_modules/(?!(react-native|@react-native|@testing-library|react-native-paper|react-clone-referenced-element|expo-status-bar|expo)/)',
-    ],
-  };
-  
